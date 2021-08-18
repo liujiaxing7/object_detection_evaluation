d612e76fd4cf8b8c68b528aac74c0ec1460beea5
liujiaxing7 <1445101363@qq.com>
2021-08-18 17:15:46 +0800

    update

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QMainWindow, QMessageBox
 from PyQt5 import QtCore, QtGui
 from src.ui.main_ui import Ui_Dialog as Main_UI
+from src.datasets.draw import draw_all
+import matplotlib
+matplotlib.use("Qt5Agg")
+from src.database.db import DBManager
 
 class Stream(QtCore.QObject):
     """Redirects console output to text widget."""
@@ -20,6 +24,8 @@ class Main_Dialog(QMainWindow, Main_UI):
         self.current_directory = os.path.dirname(os.path.realpath(__file__))
         # Define error msg dialog
         self.msgBox = QMessageBox()
+        self.gridlayout = QtWidgets.QGridLayout(self.groupBox)
+        self.groupBox.setVisible(False)
 
 
         # Default values
@@ -30,10 +36,12 @@ class Main_Dialog(QMainWindow, Main_UI):
         self.filepath_classes_det = None
         self.dir_save_results = None
         self.ret = ''
+        self.process_method=''
+        self.result_csv=None
 
         self.center_screen()
-
         sys.stdout = Stream(newText=self.onUpdateText)
+
 
     def onUpdateText(self, text):
         """Write console output to text widget."""
@@ -71,8 +79,15 @@ class Main_Dialog(QMainWindow, Main_UI):
         self.msgBox.setStandardButtons(buttons)
         return self.msgBox.exec()
 
+    def comboSelectionChanged(self,index):
+        text = self.combobox_process.itemText(index)
+        if text=='yolov3':
+            self.process_method='yolov3'
+        elif text=='yolov5':
+            self.process_method='yolov5'
+
     def load_annotations_gt(self):
-        print()
+
         if self.rad_gt_format_coco_json.isChecked():
             self.ret='coco'
         elif self.rad_gt_format_pascalvoc_xml.isChecked():
@@ -83,15 +98,7 @@ class Main_Dialog(QMainWindow, Main_UI):
             print("no format select")
             exit(-1)
 
-
-    # def validate_det_choices(self):
-    #     pass
-    #
-    # def load_annotations_det(self):
-    #     pass
-
     def btn_gt_statistics_clicked(self):
-
         pass
 
     def btn_gt_dir_clicked(self):
@@ -134,7 +141,6 @@ class Main_Dialog(QMainWindow, Main_UI):
             self.txb_gt_images_dir.setText(directory)
             self.dir_images_gt = directory
 
-
     def btn_det_classes_clicked(self):
         filepath = QFileDialog.getOpenFileName(self, 'Choose a file with a list of classes',
                                                self.current_directory,
@@ -161,9 +167,6 @@ class Main_Dialog(QMainWindow, Main_UI):
         else:
             self.dir_dets = None
 
-    # def btn_statistics_det_clicked(self):
-    #     pass
-
     def btn_output_dir_clicked(self):
         if self.txb_output_dir.text() == '':
             txt = self.current_directory
@@ -177,18 +180,40 @@ class Main_Dialog(QMainWindow, Main_UI):
         else:
             self.dir_save_results = None
 
+    def btn_draw_clicked(self):
+       self.process.setVisible(False)
+       self.groupBox.setVisible(True)
+       plt= DBManager().draw()
+       self.gridlayout.addWidget(plt, 0, 2)
+
+    def btn_save_clicked(self):
+        if self.result_csv is None:
+            print("error")
+        else:
+            result_csv = self.result_csv.split("\n")
+            result_csv = result_csv[2].split(",")
+            map = float(result_csv[0])
+            recall = float(result_csv[1])
+            F1_ = float(result_csv[2])
+            Precision = float(result_csv[3])
+            tp = float(result_csv[4])
+            fp = float(result_csv[5])
+            fn = float(result_csv[6])
+
+            DBManager().add_item(self.process_method, map, recall, Precision, F1_,tp,fp,fn)
+
     def btn_run_clicked(self):
         if self.rad_gt_format_coco_json.isChecked():
             self.ret='coco'
-        elif self.rad_gt_format_pascalvoc_xml.isChecked():
+        elif self.rad_gt_format_pascalvoc_xml.isChecked() or self.rad_gt_format_labelme_xml.isChecked():
             self.ret='voc'
         elif self.rad_gt_format_yolo_text.isChecked():
             self.ret='darknet'
         if self.ret=='':
             print("no format select")
             exit(-1)
-        evaluation = onnx.ONNX(self.dir_annotations_gt, 64, self.dir_images_gt, self.filepath_classes_gt,self.ret)
-        evaluation.evaluate()
+        evaluation = onnx.ONNX(self.dir_annotations_gt, 64, self.dir_images_gt, self.filepath_classes_gt,self.ret,self.process_method)
+        self.result_csv=evaluation.evaluate()
 if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
 
--- onnx.py
+++ onnx.py
@@ -8,9 +8,11 @@
 @desc:
 '''
 import os
-from src.utils.preprocess import pre_process as yoloPreProcess
-from src.utils.postprocess import IMAGE_SIZE_YOLOV3, THRESHOLD_YOLOV3, post_processing,\
-    load_class_names
+from src.utils.process.yolov3.preprocess_yolov3 import pre_process as yoloPreProcess_yolov3
+from src.utils.process.yolov5.preprocess_yolov5 import pre_process as yoloPreProcess_yolov5
+from src.utils.process.yolov3.postprocess_yolov3 import  THRESHOLD_YOLOV3, post_processing,\
+    load_class_names,get_prediction_yolov3
+from src.utils.process.yolov5.postprocess_yolov5 import PostProcessor_YOLOV5,IMAGE_SIZE_YOLOV5, THRESHOLD_YOLOV5,get_prediction_yolov5
 import cv2
 import onnxruntime
 from src.evaluation import *
@@ -22,7 +24,7 @@ from PIL import Image
 import numpy as np
 import src.datasets.voctodarknet as create_list
 class ONNX(object):
-    def __init__(self, file,batch_size,data_dir,classes,ret):
+    def __init__(self, file,batch_size,data_dir,classes,ret,process_method):
         if os.path.isfile(file):
             self.session = onnxruntime.InferenceSession(file)
             self.predictions=[]
@@ -31,6 +33,7 @@ class ONNX(object):
             self.data_dir=data_dir
             self.classes_path=classes
             self.format=ret
+            self.process_method=process_method
 
         else:
             raise IOError("no such file {}".format(file))
@@ -38,53 +41,48 @@ class ONNX(object):
     def forward(self, image):
         oriY = image.shape[0]
         oriX = image.shape[1]
-        image = cv2.resize(image, (IMAGE_SIZE_YOLOV3, IMAGE_SIZE_YOLOV3), interpolation=cv2.INTER_LINEAR)
-        image = yoloPreProcess(image)
+
+        if self.process_method=='yolov3':
+            image = yoloPreProcess_yolov3(image)
+        elif self.process_method=='yolov5':
+            image = yoloPreProcess_yolov5(image)
+
         input_name = self.session.get_inputs()[0].name
         outputs = self.session.run(None, {input_name: image})
-        boxes = post_processing(image, THRESHOLD_YOLOV3, 0.6, outputs)
 
-        box_es=[]
-        labels=[]
-        scores=[]
-        for i in boxes[0]:
-            box_es.append([i[0]*oriX,i[1]*oriY,i[2]*oriX,i[3]*oriY])
-            labels.append(i[6])
-            scores.append(i[4])
-        prediction={'boxes':box_es,'labels':labels,'scores':scores}
-        self.predictions.append(prediction)
+        if self.process_method=='yolov3':
+            boxes = post_processing(image, THRESHOLD_YOLOV3, 0.6, outputs)
+            prediction=get_prediction_yolov3(boxes,oriX,oriY)
+            self.predictions.append(prediction)
 
-    def load_class_names(self,namesfile):
-        class_names = []
-        with open(namesfile, 'r') as fp:
-            lines = fp.readlines()
-        for line in lines:
-            line = line.rstrip()
-            class_names.append(line)
-        return class_names
+        elif self.process_method=='yolov5':
+            boxes = PostProcessor_YOLOV5(outputs)
+            prediction=get_prediction_yolov5(boxes,oriX,oriY)
+            self.predictions.append(prediction)
+
+
+
+
     def evaluate(self):
         self.classes=load_class_names(self.classes_path)
         create_list.voctodark(self.data_dir,self.classes)
-        with open(self.data_dir+'/trainval.txt', 'r') as fi:
-            data_list = fi.read().splitlines()  # These are all cleaned out
-            fi.close()
 
-        self.datasets = src.build_dataset(data_list,self.classes, 'Dataset', self.data_dir,
+        self.datasets = src.build_dataset(self.classes, 'Dataset', self.data_dir,
                                           None, None, None, True)
-        for i in range(200):
+        output_dir='./result/'+self.process_method
+        for i in range(10):
             image_id, annotation = self.datasets.get_file(i)
             image = np.array(Image.open(image_id))
             # size = image.size()
             # s = image.bits().asstring(size.width() * size.height() * image.depth() // 8)  # format 0xffRRGGBB
             # image = np.fromstring(s, dtype=np.uint8).reshape((size.height(), size.width(), image.depth() // 8))
-            gray = image[:, :, 0]
-            gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
-            self.forward(gray)
+            self.forward(image)
 
         if self.format=='voc':
-            xml.evaluation(self.datasets, self.predictions, "./", False, None, None)
+            result_csv=xml.evaluation(self.datasets, self.predictions, output_dir, False, None, None)
         if self.format=='coco':
-            xml.evaluation_coco(self.datasets, self.predictions, "./", False, None, None)
+            result_csv=xml.evaluation_coco(self.datasets, self.predictions, output_dir, False, None, None)
         if self.format=='darknet':
-            xml.evaluation_darknet(self.datasets, self.predictions, "./", False, None, None)
+            result_csv=xml.evaluation_darknet(self.datasets, self.predictions,output_dir,False, None, None)
+        return result_csv
 
