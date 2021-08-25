042a74233e2e54bc856fcb3fd17d80c04cc82eec
liujiaxing7 <1445101363@qq.com>
2021-08-25 10:23:18 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
-import matplotlib
-matplotlib.use("Qt5Agg")
+from PyQt5 import QtWidgets
 from src.ui.visual_ui import Ui_Window
 
 if __name__=='__main__':
