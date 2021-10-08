b111715de68ddfef5d11922bcb76b433567d3fba
liujiaxing7 <1445101363@qq.com>
2021-10-08 17:07:14 +0800

    update

--- onnx.py
+++ onnx.py
@@ -80,7 +80,6 @@ class ONNX(object):
 
         output_dir='./result/'+self.process_method
         batch_size=len(open(os.path.join(self.data_dir, "ImageSets", "Main", "test.txt" )).readlines())
-        batch_size=200
         for i in tqdm(range(batch_size)):
             image_id, annotation = self.datasets.get_file(i)
             image = np.array(Image.open(image_id))
