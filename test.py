9f90f4ebeff10cc6c502813364e4c4fc74676882
DLTensor <1523452426@qq.com>
2021-12-20 17:29:46 +0800

    support export error by classes

--- test.py
+++ test.py
@@ -1,35 +1,51 @@
-# import argparse
-# import logging
-# import os
-#
-#
-# import onnx
-#
-# def main():
-#     parser = argparse.ArgumentParser(description='Onnx Model Evaluation support VOC,COCO and Darknet dataset.')
-#
-#     parser.add_argument("--model_path",help="The path to the onnx model for test",default="./config/model/yolov3.onnx",type=str,)
-#     parser.add_argument("--batchsize",help="The batchsize",default=128,type=int,)
-#     parser.add_argument("--data_dir", default="/home/fandong/Code/Evaluation-procedure/datapig", type=str, help="The directory to store evaluation results.")
-#     # parser.add_argument("--class_names", default=[], type=list, help="The classes of datasets.")
-#     parser.add_argument("--data_format", default='darknet', type=str, choices=['voc','coco','darknet'])
-#     parser.add_argument("--classes", default="./config/classes.names"
-#                         , type=str, help="The directory to get the classes of datasets")
-#
-#     args = parser.parse_args()
-#
-#     num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
-#     distributed = num_gpus > 1
-#
-#     if torch.cuda.is_available():
-#         # This flag allows you to enable the inbuilt cudnn auto-tuner to
-#         # find the best algorithm to use for your hardware.
-#         torch.backends.cudnn.benchmark = True
-#     if distributed:
-#         torch.cuda.set_device(args.local_rank)
-#         torch.distributed.init_process_group(backend="nccl", init_method="env://")
-#     evaluation=onnx.ONNX(args.model_path,args.batchsize,args.data_dir,args.classes,args.data_format)
-#     evaluation.evaluate()
-#
-# if __name__ == '__main__':
-#     main()
+import csv
+import os
+
+from PyQt5 import QtSql
+
+from src.database.db import DBManager
+
+result_csv = []
+
+db_text = os.getcwd() + '/src/database/core'
+
+# 添加一个sqlite数据库连接并打开
+db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+db.setDatabaseName('{}.db'.format(db_text))
+db.open()
+DB = DBManager()
+with open('result_2021-10-09_18-52-24.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        result_csv.append(row)
+
+# each class
+class_name = result_csv[4]
+model_name = 'yolov3_best'
+dataset_name = 'subDataset'
+AP_class, F1_class, prec_class, rec_class, threshold_class, TP_class, FP_class, FN_class = result_csv[
+                                                                                               6], result_csv[7], \
+                                                                                           result_csv[8], \
+                                                                                           result_csv[
+                                                                                               9], \
+                                                                                           result_csv[
+                                                                                               10] \
+    , result_csv[11], result_csv[12], result_csv[13]
+
+TP_all, FP_all, FN_all = 0, 0, 0
+
+for i, name_cl in enumerate(class_name):
+    if name_cl == 'class_names ' or name_cl == '':
+        continue
+    else:
+        map_cl = 0
+        ap_cl, tp_cl, fp_cl, fn_cl, F1_cl, prec_cl, rec_cl, thre_cl = AP_class[i], TP_class[i], FP_class[i], \
+                                                                      FN_class[i], F1_class[i], prec_class[
+                                                                          i], rec_class[i], threshold_class[
+                                                                          i]
+        TP_all += int(tp_cl)
+        FP_all += int(fp_cl)
+        FN_all += int(fn_cl)
+
+    DB.addItem(model_name, dataset_name, name_cl, tp_cl, fp_cl, fn_cl, F1_cl, ap_cl, map_cl, prec_cl,
+                rec_cl, thre_cl)
